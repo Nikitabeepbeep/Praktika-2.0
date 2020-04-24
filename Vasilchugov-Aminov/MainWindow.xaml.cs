9a5873acbc2d7638f04d35057ef27f8557db1bdb
Никита Васильчугов <nikitoscocos@gmail.com>
2020-04-24 20:51:31 +0500

    Добавьте файлы проекта.

---
+++ Vasilchugov-Aminov/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vasilchugov_Aminov
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //присоедение к БД
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //файл
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //настройки
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //о программе
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //очистить
+        private void Button_Click4(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
