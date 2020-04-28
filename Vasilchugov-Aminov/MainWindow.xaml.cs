cb16073df2d03dbbed07a20576f3a79beec9ea35
Никита Васильчугов <nikitoscocos@gmail.com>
2020-04-28 22:03:16 +0500

    Добавлено "интерактивное меню"

--- Vasilchugov-Aminov/MainWindow.xaml.cs
+++ Vasilchugov-Aminov/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using NLog;
+﻿using Microsoft.Win32;
+using NLog;
+using System;
 using System.Text;
 using System.Windows;
 namespace Vasilchugov_Aminov
@@ -9,25 +11,6 @@ namespace Vasilchugov_Aminov
     public partial class MainWindow : Window
 
     {
-        private static Logger logger = LogManager.GetCurrentClassLogger();
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-        //присоедение к БД
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            DBConnectionForm taskWindow = new DBConnectionForm();
-            taskWindow.Owner = this;
-            taskWindow.Show();
-        }
-        //файл
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            AddTxtFile addTxtFile = new AddTxtFile();
-            addTxtFile.Show();
-                    
-        }
         //настройки
         private void Button_Click2(object sender, RoutedEventArgs e)
         {
@@ -41,8 +24,8 @@ namespace Vasilchugov_Aminov
             sb.AppendLine("Программа пока непонятно о чём");
             sb.AppendLine("Сделана двумя студентами из группы 9ИСП-391к-17");
             sb.AppendLine("Создатели: ");
-            sb.AppendLine("Аминов В.Т(9ИСП-391к-17)");
-            sb.AppendLine("Васильчугов Н.А(9ИСП-391к-17)");
+            sb.AppendLine("Аминов В.Т");
+            sb.AppendLine("Васильчугов Н.А");
             sb.AppendLine("Наша ссылка на GitHub:");
             sb.AppendLine("https://github.com/Nikitabeepbeep/Praktika-2.0");
             MessageBox.Show(sb.ToString());
@@ -54,6 +37,34 @@ namespace Vasilchugov_Aminov
         }
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
+
+        }
+        private void MenuItem_Click1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //присоединение к базе данных
+        private void Baza_dannyx(object sender, RoutedEventArgs e)
+        {
+            DBConnectionForm taskWindow = new DBConnectionForm();
+            taskWindow.Owner = this;
+            taskWindow.Show();
+        }
+        private void newbutton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                    kekk.Items.Add(System.IO.Path.GetFileName(filename));
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   
 
         }
     }
